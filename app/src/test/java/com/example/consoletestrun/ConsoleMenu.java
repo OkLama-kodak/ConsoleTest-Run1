3ccb4b209e2126d08658edeba15462c0a71ef4e9
rv castro <423004999@ntc.edu.ph>
2026-09-19 15:55:41 +0800

    Added tester 101

--- app/src/test/java/com/example/consoletestrun/ConsoleMenu.java
+++ app/src/test/java/com/example/consoletestrun/ConsoleMenu.java
@@ -18,7 +18,7 @@ public class ConsoleMenu {
                     System.out.println("\n -> Starting a new game...");
                     break;
                 case 2:
-                    System.out.println("\n -> Loa ding settings...");
+                    System.out.println("\n -> Loading settings...");
                     break;
                 case 3:
                     System.out.println("\n -> Displaying leaderboard...");
